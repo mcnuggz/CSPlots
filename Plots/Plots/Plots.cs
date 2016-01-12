b5652c18254e0ec09d3211ebaf498b36d669e1a4
Ryan Webb <dzanfox@gmail.com>
2016-01-12 13:40:38 -0600

    got the arrays to work

---
+++ Plots/Plots/Plots.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Plots
+{
+    public class Plots
+    {
+        protected int x;
+        protected int y;
+        protected int width;
+        protected int height;
+
+        string plot = File.ReadAllText("Plot.txt");
+        public int[] plotInts;
+
+        public void Print()
+        {
+
+            foreach (var item in plotInts)
+            {
+                Console.Write(item.ToString() + " ");
+            }
+
+        }
+
+        public void ConvertToInt()
+        {
+            string[] tokens = plot.Split(',');
+
+            plotInts = Array.ConvertAll<string, int>(tokens, int.Parse);
+
+        }
+
+    }
+}
--- Plots/Plots/Program.cs
+++ Plots/Plots/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Plots
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Plots plot = new Plots();
+            plot.ConvertToInt();
+            plot.Print();
+            Console.ReadLine();
         }
     }
 }
